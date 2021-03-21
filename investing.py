166be4f40344a0e3ac965d68749cef11ad304946
Kab2k <casper.kristiansson@yahoo.se>
2021-03-21 13:20:34 +0100

    Fixed Adjustment Formula

--- investing.py
+++ investing.py
@@ -16,7 +16,7 @@ def plot_graph(months, data_with_rent, data_without_rent, acctual_value, acctual
     fig.add_trace(go.Scatter(x=months, y=data_with_rent,
                     mode='lines+markers',
                     name='With Rent'))
-    
+
     fig.add_trace(go.Scatter(x=months, y=data_without_rent,
                     mode='lines+markers',
                     name='Without Rent'))
@@ -28,7 +28,7 @@ def plot_graph(months, data_with_rent, data_without_rent, acctual_value, acctual
     fig.add_trace(go.Scatter(x=months, y=acctual_value_rent,
                     mode='lines+markers',
                     name='Acctual Value Rent'))
-    
+
     fig.add_trace(go.Scatter(x=months, y=rent,
                     mode='lines+markers',
                     name='Rent'))
@@ -44,19 +44,18 @@ def plot_graph(months, data_with_rent, data_without_rent, acctual_value, acctual
     fig2.add_trace(go.Scatter(x=months, y=acctual_adjust,
                     mode='lines+markers',
                     name='Acctual Value'))
-    
+
     fig2.add_trace(go.Scatter(x=months, y=adjust_rent,
                     mode='lines+markers',
                     name='New Rent'))
-    
-    fig2.show()
+
+    #fig2.show()
 
 def growth_with_rent(months_length):
     data_exponential = []
     acctual_value = []
     rent_data = []
     i = 0
-    j = 0
     start_amount = 6315.167     # 75782/år = 6315.167kr/mån
     current_amount = 0
     exponential_growth = 1.01
@@ -73,7 +72,7 @@ def growth_with_rent(months_length):
             start_amount = 0
             rent = 0
         acctual_value.append(current_amount - adjustment)
-        
+
         rent_data.append((-rent*(i+1)))
 
         i+=1
@@ -84,7 +83,6 @@ def growth_without_rent(months_length):
     data_exponential = []
     acctual_value = []
     i = 0
-    j = 0
     start_amount = 6315.167     # 75782/år = 6315.167kr/mån
     current_amount = 0
     exponential_growth = 1.01
@@ -110,11 +108,9 @@ def adjust_winnings(months_length):
     acctual_value = []
     rent_data = []
     i = 0
-    j = 0
     start_amount = 6315.167     # 75782/år = 6315.167kr/mån
     current_amount = 0
-    exponential_growth = 1.01
-    rent = 2250                 # 27000kr/år = 2250kr/mån
+    exponential_growth = 1.01   # 27000kr/år = 2250kr/mån
     adjustment = 0
     last_amount = 6315.167
     total_rent = 0
@@ -124,10 +120,9 @@ def adjust_winnings(months_length):
             adjustment += start_amount
         elif i == 36:
             start_amount = 0
-            rent = 0
 
         current_amount = current_amount*exponential_growth + start_amount
-        change_amount = (current_amount - last_amount) * 0.2
+        change_amount = (current_amount - last_amount - start_amount) * 0.2
 
         current_amount -= change_amount
         total_rent += change_amount
